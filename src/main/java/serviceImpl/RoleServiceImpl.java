01292572157d9ef7f606af8163967660fb95184c
Gana <Appu@192.168.11.16>
2016-02-24 18:03:46 +0800

    role service

--- src/main/java/serviceImpl/RoleServiceImpl.java
+++ src/main/java/serviceImpl/RoleServiceImpl.java
@@ -28,8 +28,10 @@ public class RoleServiceImpl implements RoleService {
 	}
 
 	private RoleSendto toRoleSendto(Role role) {
+		RoleSendto ret = new RoleSendto();
+		ret.setId(role.getId());
 
-		return null;
+		return ret;
 	}
 
 	@Override
